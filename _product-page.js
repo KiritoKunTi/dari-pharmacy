6989e8b86a95a6a41fbdfecb036e24bc6b624541
Aisayev Duman <duman200354@mail.ru>
2022-05-05 18:05:54 +0600

    login page

--- _product-page.js
+++ _product-page.js
@@ -27,7 +27,7 @@ const ctx = document.getElementById('myChart').getContext('2d');
 const myChart = new Chart(ctx, {
     type: 'line',
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: ['12'],
         datasets: [{
             label: 'Price Evolution',
             data: [12, 19, 3, 5, 2, 3],
