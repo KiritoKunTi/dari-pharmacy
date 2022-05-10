cd79638b93845c363a87517bf5b1fdde6607f9e6
Aisayev Duman <duman200354@mail.ru>
2022-05-10 12:20:56 +0600

    buy popup

--- profile/js/basket.js
+++ profile/js/basket.js
@@ -1,18 +1,33 @@
 const closeBtn = document.getElementById('js-close-btn');
 const deleteCancel = document.getElementById('js-delete-cancel');
-const nodePopUp = document.querySelector('.delete-popup-bg');
+const nodeDeletePopUp = document.querySelector('.delete-popup-bg');
 
 
 function openDeletePopUP() {
-    nodePopUp.classList.add('toggleBtn');
+    nodeDeletePopUp.classList.add('toggleBtn');
 }
 
 deleteCancel.onclick = function() {
-    nodePopUp.classList.remove('toggleBtn');
+    nodeDeletePopUp.classList.remove('toggleBtn');
 }
 
 closeBtn.onclick = function() {
-    nodePopUp.classList.remove('toggleBtn');
+    nodeDeletePopUp.classList.remove('toggleBtn');
 }
 
+
+const nodeBuyPopUp = document.querySelector('.buy-popup-bg');
+
+function openBuyPopUp() {
+    nodeBuyPopUp.classList.add('toggleBtn');
+}
+
+// window.onclick = function(event) {
+//     if(event.target == nodeBuyPopUp)  {
+//         nodeBuyPopUp.style.display = 'none';
+//     }
+//     if(event.target == nodeDeletePopUp)  {
+//         nodeDeletePopUp.classList.remove('toggleBtn');
+//     }
+// }
 
